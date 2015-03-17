6024f043c8082b074c76247dcce9d20f9264e7c3
Ryo Anazawa <anazawa@cpan.org>
2015-03-18 05:02:01 +0900

    add clone, toSGF, rewind, peek, getNextVariation, getPreviousVariation and setCurrent methods

--- sgfgrove/collection.js
+++ sgfgrove/collection.js
@@ -13,16 +13,34 @@
 
   SGFGrove.Collection.GameTree = (function () {
     var GameTree = function (tree, parent) {
+      this.tree = tree || [[{}], []];
       this.parent = parent;
       this.gameTree = this;
-      this.sequence = tree[0];
+      this.sequence = this.tree[0];
       this.index = 0;
-      this.variations = tree[1];
+      this.variations = this.tree[1];
       this.variationIndex = 0;
     };
 
     GameTree.prototype.buildGameTree = function (tree, parent) {
       return new GameTree( tree, parent );
+    };
+
+    GameTree.prototype.clone = function () {
+      var clone = this.buildGameTree( this.tree, this.parent );
+
+      clone.index = this.index;
+      clone.variationIndex = this.variationIndex;
+
+      if ( this.gameTree !== this ) {
+        clone.gameTree = this.gameTree.clone();
+      }
+
+      return clone;
+    };
+
+    GameTree.prototype.toSGF = function () {
+      return this.tree;
     };
 
     GameTree.prototype.accept = function (visitor) {
@@ -32,6 +50,13 @@
       }
     };
 
+    GameTree.prototype.rewind = function () {
+      this.gameTree = this;
+      this.index = 0;
+      this.variationIndex = 0;
+      return this;
+    };
+
     GameTree.prototype.getNext = function () {
       var gameTree = this.gameTree;
 
@@ -39,66 +64,89 @@
         return gameTree.sequence[gameTree.index++];
       }
 
-      while ( gameTree ) {
-        if ( gameTree.variationIndex < gameTree.variations.length ) {
-          this.gameTree = this.buildGameTree(
-            gameTree.variations[gameTree.variationIndex++],
-            gameTree
-          );
-          return this.getNext();
-        }
-        gameTree = gameTree.parent;
+      if ( gameTree.variationIndex < gameTree.variations.length ) {
+        this.gameTree = this.buildGameTree(
+          gameTree.variations[gameTree.variationIndex++],
+          gameTree
+        );
+        return this.getNext();
       }
 
       return;
     };
 
     GameTree.prototype.hasNext = function () {
-       var gameTree = this.gameTree;
-
-      if ( gameTree.index < gameTree.sequence.length ) {
-        return true;
-      }
-
-      while ( gameTree ) {
-        if ( gameTree.variationIndex < gameTree.variations.length ) {
-          return true;
-        }
-        gameTree = gameTree.parent;
-      }
-
-      return false;
+      return this.gameTree.index < this.gameTree.sequence.length ||
+             this.gameTree.variationIndex < this.gameTree.variations.length;
     };
 
-    // XXX
-    GameTree.prototype.getPrevious = function () {
+    GameTree.prototype.peek = function () {
       var gameTree = this.gameTree;
-      var tree;
 
-      if ( gameTree.index > 0 ) {
-        return gameTree.sequence[--gameTree.index];
+      if ( gameTree.index < gameTree.sequence.length ) {
+        return gameTree.sequence[gameTree.index];
       }
 
-      if ( gameTree = gameTree.parent ) {
-        if ( gameTree.variationIndex > 1 ) {
-          gameTree.variationIndex--;
-          tree = this.buildGameTree( gameTree.variations[gameTree.variationIndex-1] );
-          tree.getLast();
-          tree.parent = gameTree;
-          gameTree = tree.gameTree;
-        }
-        this.gameTree = gameTree;
-        return this.getPrevious();
+      if ( gameTree.variationIndex < gameTree.variations.length ) {
+        return gameTree.variations[gameTree.variationIndex][0][0];
       }
 
       return;
     };
 
+    GameTree.prototype.getNextVariation = function () {
+      var gameTree = this.gameTree;
+      var variation;
+
+      while ( gameTree ) {
+        if ( gameTree.variationIndex < gameTree.variations.length ) {
+          variation = gameTree.variations[gameTree.variationIndex++];
+          this.gameTree = this.buildGameTree( variation, gameTree );
+          this.getNext();
+          break;
+        }
+        gameTree = gameTree.parent;
+      }
+
+      return variation;
+    };
+
+    GameTree.prototype.getPrevious = function () {
+      var gameTree = this.gameTree;
+
+      if ( gameTree.index > 1 ) {
+        gameTree.index -= 1;
+        return gameTree.sequence[gameTree.index-1];
+      }
+
+      return;
+    };
+
+    GameTree.prototype.getPreviousVariation = function () {
+      var gameTree = this.gameTree.parent;
+      var variation;
+
+      if ( !gameTree ) {
+        return;
+      }
+
+      if ( gameTree.variationIndex > 1 ) {
+        gameTree.variationIndex--;
+        while ( gameTree.variations.length ) {
+          variation = gameTree.variations[gameTree.variationIndex-1];
+          gameTree = this.buildGameTree( variation, gameTree );
+          gameTree.index = gameTree.sequence.length;
+          gameTree.variationIndex = gameTree.variations.length;
+        }
+      }
+
+      this.gameTree = gameTree;
+
+      return variation || this.getCurrentVariation();
+    };
+
     GameTree.prototype.getFirst = function () {
-      this.gameTree = this;
-      this.index = 0;
-      this.variationIndex = 0;
-      return this.getNext();
+      return this.rewind().getNext();
     };
 
     GameTree.prototype.getLast = function () {
@@ -119,6 +167,10 @@
       return this.gameTree.sequence[this.getCurrentIndex()];
     };
 
+    GameTree.prototype.setCurrent = function (node) {
+      this.gameTree.sequence[this.getCurrentIndex()] = node;
+    };
+
     GameTree.prototype.getCurrentVariation = function () {
       var parent = this.gameTree.parent || this.gameTree;
       return parent.variations[this.getCurrentVariationIndex()];
@@ -133,6 +185,9 @@
       }
 
       return index;
+    };
+
+    GameTree.prototype.setAbsoluteIndex = function (index) {
     };
 
     GameTree.prototype.getAbsoluteVariationIndex = function () {
@@ -201,10 +256,6 @@
       return !this.parent && this.getCurrentIndex() === 0;
     };
 
-    GameTree.prototype.replace = function (node) {
-      this.gameTree.sequence[this.getCurrentIndex()] = node;
-    };
-
     GameTree.prototype.remove = function () {
       var gameTree = this.gameTree;
       var parent = gameTree.parent;
@@ -252,24 +303,37 @@
     };
 
     GameTree.prototype.push = function (node) {
-      while ( !this.isLeaf() ) { this.getNext(); }
-      this.gameTree.sequence.push( node );
-      this.getNext();
+      var that = this;
+
+      if ( !that.isLeaf() ) {
+        that = that.clone();
+        that.getLast();
+      }
+
+      that.gameTree.sequence.push(node);
+
+      return;
     };
 
-    GameTree.prototype.pop = function () {
-      while ( !this.isLeaf() ) { this.getNext(); }
-      return this.remove();
+    GameTree.prototype.pop = function (node) {
+      var that = this;
+
+      if ( !that.isLeaf() ) {
+        that = that.clone();
+        that.getLast();
+      }
+
+      return that.remove();
     };
 
-    GameTree.prototype.shift = function () {
-      this.getFirst();
-      return this.remove();
+    GameTree.prototype.shift = function (node) {
+      var that = this.isRoot() ? this : this.buildGameTree( this.tree );
+      return that.remove();
     };
 
     GameTree.prototype.unshift = function (node) {
-      this.getFirst();
-      this.insert(node);
+      var that = this.isRoot() ? this : this.buildGameTree( this.tree );
+      return that.insert(node);
     };
 
     return GameTree;
