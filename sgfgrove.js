4cf2e137eac3dfdc1a4540394f76d72c8e06750c
Ryo Anazawa <anazawa@cpan.org>
2016-03-24 12:35:58 +0900

    version 1.0.3

--- sgfgrove.js
+++ sgfgrove.js
@@ -1,5 +1,5 @@
 /**
- * @license SGFGrove.js 1.0.2
+ * @license SGFGrove.js 1.0.3
  * (c) 2015-2016 Ryo Anazawa https://github.com/anazawa/sgfgrove
  * Licence: MIT
  */
@@ -7,7 +7,7 @@
     "use strict";
 
     var SGFGrove = {
-        VERSION: "1.0.2"
+        VERSION: "1.0.3"
     };
 
     SGFGrove.Util = (function () {
