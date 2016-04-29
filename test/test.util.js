d48b29c882fef60945d68637901717ddbe90f177
Ryo Anazawa <anazawa@cpan.org>
2016-04-29 15:06:52 +0900

    remove tests from Util.isArray

--- test/test.util.js
+++ test/test.util.js
@@ -10,14 +10,6 @@
      *  http://underscorejs.org/
      */
 
-    test("SGFGrove.Util.isArray", function (t) {
-        var isArray = SGF.Util.isArray;
-        t.ok(!isArray(void 0), "undefined vars are not arrays");
-        t.ok(!isArray(arguments), "the arguments object is not an array");
-        t.ok(isArray([1, 2, 3]), "but arrays are");
-        t.end();
-    });
-
     test("SGFGrove.Util.isNumber", function (t) {
         var isNumber = SGF.Util.isNumber;
         t.ok(!isNumber("string"), "a string is not a number");
